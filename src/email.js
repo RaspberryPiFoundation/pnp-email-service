27f92c85d103fe8f7db0c503264d6fcda4f17620
Wardormeur <wardormeur@gmail.com>
2018-06-15 14:53:48 +0100

    rename missingLanguageFallback

--- src/email.js
+++ src/email.js
@@ -35,11 +35,11 @@ module.exports = env => {
   const processIfExists = async ({ filename, formatter }, data, func) => {
     const fullPathFile = fullPath(filename);
     const originalPathExists = await fileExists(fullPathFile);
-    const alternativeFullPathFile = fullPath(formatter(env('missingLanguageFallback')()));
+    const alternativeFullPathFile = fullPath(formatter(env('languageFallback')()));
     const alternativePathExists = await fileExists(alternativeFullPathFile);
     return originalPathExists ?
       func(fullPathFile, data) :
-        env('missingLanguageFallback') && alternativePathExists ?
+        env('languageFallback') && alternativePathExists ?
           func(alternativeFullPathFile, data) :
           void 0;
   }
